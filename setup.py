125847871951f0ea041b71a3738ff0c2804925b7
Grzegorz Skoraczynski <g.skoraczynski@mimuw.edu.pl>
2021-07-13 22:33:37 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='LC/GC-MS alignment algorithm based on Wasserstein distance',
+    author='Grzegorz Skoraczynski',
+    license='MIT',
+)
