7665f6e910206be17c52ef7a39b2ae5b3362af20
Grzegorz Skoraczynski <gskoraczynski@gmail.com>
2022-08-15 21:48:02 +0200

    Paralelization inside preclusters

--- Alignstein/__init__.py
+++ Alignstein/__init__.py
@@ -1,9 +1,11 @@
 from .align import *
-from .multialign import cluster_mids, find_consensus_features
+from .multialign import (cluster_mids, find_consensus_features, gather_mids,
+                         find_consensus_features_paralel)
 
 from .chromatogram import Chromatogram
 
 from .parse import (detect_features_from_file, parse_chromatogram_file,
                     find_features, features_to_weight,
                     parse_chromatogram_with_detected_features,
-                    openms_features_to_features)
+                    openms_features_to_features,
+                    openms_feature_to_feature)
--- Alignstein/multialign.py
+++ Alignstein/multialign.py
@@ -1,8 +1,16 @@
+from concurrent.futures import ProcessPoolExecutor
+from contextlib import closing
+from itertools import repeat
+from typing import List, Any
+
 import numpy as np
 from sklearn.cluster import MiniBatchKMeans, AgglomerativeClustering
 
+from .chromatogram import Chromatogram
 from .align import gwd_distance_matrix, gwd_distance_matrix_parallel
 from .mfmc import match_chromatograms_gathered_by_clusters
+
+_BIG_CLUSTER_COUNT = 16
 
 
 def gather_mids(feature_sets_list):
@@ -88,29 +96,34 @@ def find_consensus_features(clusters, feature_sets_list,
     -------
     Complicated consensus features.
     """
+    # TODO Refactor this function
     consensus_features = [[[] for _ in range(len(np.unique(clusters)))]
                           for _ in range(turns)]
-    clusters = clusters.astype(int)
+    import time
     for sample_i, one_sample_features in enumerate(feature_sets_list):
         rest_of_features, clusters_filtered = flatten_chromatograms(
             feature_sets_list, clusters,
             exclude_chromatogram_sets=[sample_i])
+        start = time.time()
         dists = gwd_distance_matrix_parallel(
             one_sample_features, rest_of_features,
             centroid_upper_bound=centroid_upper_bound,
             gwd_upper_bound=gwd_upper_bound,
             mz_mid_upper_bound=mz_mid_upper_bound,
             eps=eps)
+        print("Dists calculated in:", time.time() - start)
 
         for turn in range(turns):
+            start = time.time()
             matchings, matched_left, matched_right = \
                 match_chromatograms_gathered_by_clusters(
                     dists, clusters_filtered, matching_penalty)
+            print("Matching done in:", time.time() - start)
             if len(matchings) == 0:
                 print("Breaking at turn ", turn)
                 break  # there is nothing more to be matched in next turns
             for feature_j, cluster in matchings:
-                consensus_features[turn][cluster].append((sample_i, feature_j))
+                consensus_features[turn][int(cluster)].append((sample_i, feature_j))
             dists[list(matched_left)] = np.inf  # simply stupid way to omit
             # already used chromatograms (indeed, maybe confusing, but previous
             # line changes whole rows)
@@ -123,12 +136,86 @@ def find_consensus_features(clusters, feature_sets_list,
     return succeeded_consensus_features
 
 
+def find_consensus_features_paralel(clusters, feature_sets_list,
+                                    centroid_upper_bound=10, gwd_upper_bound=10,
+                                    matching_penalty=5, turns=10,
+                                    mz_mid_upper_bound=float("inf"), eps=0.1,
+                                    big_clusters=None):
+    """
+    Find consensus feature in preclustered dataset paralelized version.
+
+    This function in comparison with find_consensus_features additionally split
+    input sets by big clusters and runs it paralely. It does not fall much in
+    precision but reduces polynomial problems to smallers and paralelizes
+    execution.
+
+    Parameters
+    ----------
+    clusters
+    feature_sets_list
+    centroid_upper_bound : float
+        Maximum cetroid distance between which GWD will computed. For efficiency
+        reasons should be reasonably small.
+    gwd_upper_bound : float
+        Penalty for not transporting a part of signal, aka the lambda parameter.
+        Can be interpreted as maximal distance over which signal is
+        transported while computing GWD.
+    matching_penalty : penalty for feature not matching
+    turns : number of one feature set matching repeats
+    mz_mid_upper_bound : float
+        Additional parameter if GDW should be computed only for features with
+        centroid M/Z difference lower than this parameter. Usually not used.
+    eps : float
+        GWD entropic penalization coefficient, aka the epsilon parameter.
+        Default value is chosen reasonably. Change it only if you understand how
+        it works.
+    big_clusters : iterable of int
+        Feature matching is done within these clusters.
+
+    Returns
+    -------
+    Complicated consensus features.
+    """
+    # 1. Split inputs by big clusters
+    big_clusters = big_clusters.astype(int)
+    assert len(np.unique(big_clusters)) == np.max(big_clusters) + 1
+    big_clusters_number = len(np.unique(big_clusters))
+    feature_id_mapping: list[list[list[int]]] = [[[] for _2 in range(len(feature_sets_list))] for _ in range(big_clusters_number)]
+    features_separated: list[list[list[Chromatogram]]] = [ # features separated by big clusters
+        [
+            [] for _2 in range(len(feature_sets_list))
+        ] for _ in range(big_clusters_number)]
+    i = 0
+    for fset_id, fset in enumerate(feature_sets_list):
+        for f_id, feature in enumerate(fset):
+            features_separated[big_clusters[i]][fset_id].append(feature)
+            feature_id_mapping[big_clusters[i]][fset_id].append(f_id)
+            i += 1
+    clusters_separated = clusters
+    with ProcessPoolExecutor(max_workers=_BIG_CLUSTER_COUNT) as outer_pool:
+        separated_cfeatures = list(
+            outer_pool.map(find_consensus_features,
+                           clusters_separated, features_separated,
+                           repeat(centroid_upper_bound), repeat(gwd_upper_bound),
+                           repeat(matching_penalty), repeat(turns),
+                           repeat(mz_mid_upper_bound), repeat(eps)))
+
+    # 3. Merge results
+    consensus_features = []
+    for big_cluster_id, big_cluster_cfeatures in enumerate(separated_cfeatures):
+        for cfeature in big_cluster_cfeatures:
+            mapped_cfeature = []
+            for set_id, f_id in cfeature:
+                mapped_cfeature.append((set_id, feature_id_mapping[big_cluster_id][set_id][f_id]))
+            consensus_features.append(mapped_cfeature)
+    return consensus_features
+
 def precluster_mids(mids):
     return np.array(
-        MiniBatchKMeans(n_clusters=16, init='k-means++', max_iter=100,
-                        batch_size=100, verbose=0, compute_labels=True,
-                        random_state=None, tol=0.0, max_no_improvement=10,
-                        init_size=None, n_init=3,
+        MiniBatchKMeans(n_clusters=_BIG_CLUSTER_COUNT, init='k-means++',
+                        max_iter=100, batch_size=100, verbose=0,
+                        compute_labels=True, random_state=None, tol=0.0,
+                        max_no_improvement=10, init_size=None, n_init=3,
                         reassignment_ratio=0.01).fit_predict(mids))
 
 
@@ -144,7 +231,8 @@ def cluster_mids_subsets(mids, distance_threshold=10):
                                    ).fit_predict(mids_log)
 
 
-def big_clusters_to_clusters(mids, big_clusters, distance_threshold=5):
+def big_clusters_to_clusters(mids, big_clusters, distance_threshold=5,
+                             clusters_flat=False):
     """
     Do clustering over rough preclusters.
     Parameters
@@ -160,13 +248,20 @@ def big_clusters_to_clusters(mids, big_clusters, distance_threshold=5):
     list of ints
         Clusters
     """
-    clusters = -1 * np.ones(len(mids))
+    # TODO make it better
+    if clusters_flat:
+        clusters = -1 * np.ones(len(mids))
+    else:
+        clusters = []
     for i in range(np.max(big_clusters) + 1):
         inds = np.where(big_clusters == i)
         mids_subset = mids[inds]
         clusters_subsets = cluster_mids_subsets(
             mids_subset, distance_threshold=distance_threshold)
-        clusters[inds] = clusters_subsets + np.max(clusters) + 1
+        if clusters_flat:
+            clusters[inds] = clusters_subsets + np.max(clusters) + 1
+        else:
+            clusters.append(clusters_subsets)
     return clusters
 
 
